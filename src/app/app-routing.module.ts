5b0d2d591cec988004e8ecf7f0354fd4c26067f7
Archit Garg <archit.garg@calsoftinc.com>
2020-11-24 21:57:56 +0530

    Sign up page created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginCompComponent } from './login-comp/login-comp.component'
+import { SignupCompComponent } from './signup-comp/signup-comp.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginCompComponent},
+  {path: 'signup', component: SignupCompComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginCompComponent } from './login-comp/login-comp.component';
-import { SignupCompComponent } from './login-comp/signup-comp/signup-comp.component';
+import { SignupCompComponent } from './signup-comp/signup-comp.component';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginCompComponent, SignupCompComponent],
+  declarations: [AppComponent, LoginCompComponent, SignupCompComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
