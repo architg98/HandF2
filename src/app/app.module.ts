2fd93cb6a2abced5ead471c444e68fcd5ba0af7d
Archit Garg <archit.garg@calsoftinc.com>
2020-11-24 17:54:10 +0530

    Starter project with login and signup components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginCompComponent } from './login-comp/login-comp.component';
+import { SignupCompComponent } from './login-comp/signup-comp/signup-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginCompComponent,
+    SignupCompComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/login-comp/signup-comp/signup-comp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-comp',
+  templateUrl: './signup-comp.component.html',
+  styleUrls: ['./signup-comp.component.css']
+})
+export class SignupCompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
